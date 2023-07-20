070cbde619e6bcf6e6f37e7227a14b5ed34bc6c9
David Sosa <david.sosa@protonmail.com>
2023-07-20 19:51:54 +0200

    Added collection creation

--- place_in_shopify.py
+++ place_in_shopify.py
@@ -2,27 +2,38 @@
 import shopify
 import requests
 import base64
+import re
 from pathlib import Path
 from get_products import extract_product_info, extract_menu_links
-from upload_pdf import upload_pdf_to_shopify
 from bing_image_downloader import downloader
+from unidecode import unidecode
 import os
 
-def remove_non_ascii(string):
-    """
-    Remove non-ASCII characters from a string.
-    
-    Parameters:
-        string (str): The input string.
-        
-    Returns:
-        str: The string with non-ASCII characters removed.
-    """
-    return ''.join(char for char in string if ord(char) < 128)
+
+def replace_spaces_with_underscore(string):
+    return re.sub('\s+', '_', string)
 
 
-def download_images(query, limit=10):
-    downloader.download(query, limit=limit, output_dir="images", adult_filter_off=True, force_replace=False, timeout=60)
+def clean_string(string):
+    print(repr(string))
+    string = string.strip()
+    string = string.replace("\n", " ")
+    string = string.lower()
+    string = string.replace('®', '')
+    string = string.strip()
+    string = string.replace('/', '_')
+
+    string = replace_spaces_with_underscore(string)
+    string = string.replace(" ", "_")
+    string = unidecode(string)
+    return string
+
+
+def download_images(query, folder, additional_search, limit=10):
+    print(query, folder, additional_search)
+    downloader.download(query, folder, additional_search, limit=limit, output_dir="images",
+                        adult_filter_off=True, force_replace=False, timeout=60)
+
 
 def save_image_to_shopify(image_path, product_id, position=1):
     # Open the image file from your local directory and read its binary data
@@ -56,26 +67,48 @@ for link in extract_menu_links(old_website_url):
     response = requests.get(url)
     products = extract_product_info(response.content)
 
+    collection = shopify.SmartCollection()
+    collection.title = section
+
+    collection.rules = {
+        'column': 'type',
+        'relation': 'equals',
+        'condition': section
+    },
+
+    collection.save()
+    # https://www.mitek-us.com/
     for product in products:
         product_title = product['product_name']
+        if not product_title:
+            continue
+        product_title_cleaned = clean_string(product_title)
+
         # Create a new product
         new_product = shopify.Product()
         new_product.title = product_title
         new_product.product_type = section
+        new_product.template_suffix = "sin-precio-ficha-tecnica"
 
         if len(product['file_paths']) > 0:
             pdf_url = old_website_url+"/"+product['file_paths'][0]
-            new_product.body_html = f'<p>{product["product_description"]}</p>\n\n<p><a href={pdf_url} style="font-size: 1.875rem;" target="_blank" rel="noopener noreferrer"> Descargar Ficha Técnica</a></p>'
+            new_product.body_html = new_product.body_html = (
+                                    f'<p>{product["product_description"]}</p>\n\n'
+                                    f'<p><a href={pdf_url} style="font-size: 1.875rem;" '
+                                    f'target="_blank" rel="noopener noreferrer">'
+                                    f'Descargar Ficha Técnica</a></p>')
         else:
             new_product.body_html = f'<p>{product["product_description"]}</p>'
-        
+
         print("Saving product")
         new_product.save()
+        if "tabla" in product_title_cleaned:
+            additional_search = "USG"
+        else:
+            additional_search = None
 
-        query = product_title +" "+"USG"
-        download_images(query, limit=10)
-        for i, image in enumerate(Path(f"./images/{query}/").iterdir()):
+        download_images(product_title, product_title_cleaned, additional_search, limit=10)
+        for i, image in enumerate(Path(f"./images/{product_title_cleaned}/").iterdir()):
             save_image_to_shopify(f'{image}', new_product.id, position=i)
-            
 
-
+    break
